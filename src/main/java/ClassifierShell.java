f94d312434af27ddc9664a42cf9f15f09134b3e1
Adam Harris <adam.jensen.harris@gmail.com>
2015-05-15 20:56:08 -0600

    Added ID3, including implementation in ClassifierShell

--- src/main/java/ClassifierShell.java
+++ src/main/java/ClassifierShell.java
@@ -1,6 +1,10 @@
 import weka.classifiers.Evaluation;
 import weka.classifiers.Classifier;
 import weka.classifiers.lazy.IBk;
+import weka.classifiers.trees.Id3;
+import weka.filters.unsupervised.attribute.Discretize;
+import weka.filters.unsupervised.attribute.Standardize;
+import weka.filters.Filter;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
@@ -8,31 +12,44 @@ import java.util.Random;
 
 public class ClassifierShell {
     public static void main(String[] args) throws Exception {
-        DataSource source = new DataSource("irisdata.csv");
-        Instances dataSet = source.getDataSet();
+            DataSource source = new DataSource("house-votes-84.csv");
+            Instances dataSetPre = source.getDataSet();
 
-        dataSet.setClassIndex(dataSet.numAttributes() - 1);
+            dataSetPre.setClassIndex(dataSetPre.numAttributes() - 1);
+            //dataSetPre.setClassIndex(0);
 
-        dataSet.randomize(new Random(1));
+            Standardize stand = new Standardize();
+            stand.setInputFormat(dataSetPre);
 
-        int trainSize = (int) Math.round(dataSet.numInstances() * .7);
-        int testSize = dataSet.numInstances() - trainSize;
+            Discretize discretize = new Discretize();
+            discretize.setInputFormat(dataSetPre);
 
-        Instances train = new Instances(dataSet, 0, trainSize);
-        Instances test = new Instances(dataSet, trainSize, testSize);
+            Instances dataSet = dataSetPre;
 
-        //My home-grown KNN algorithm
-        KNN classifier = new KNN(15);
-        classifier.buildClassifier(train);
+            dataSet = Filter.useFilter(dataSet, discretize);
+            dataSet = Filter.useFilter(dataSet, stand);
 
-        //Weka's KNN Algorithm
-        //Classifier classifier = new IBk(15);
-        //classifier.buildClassifier(train);
+            dataSet.randomize(new Random(9001));
 
-        Evaluation eval = new Evaluation(train);
-        eval.evaluateModel(classifier, test);
-        System.out.println(eval.toSummaryString("\nResults\n", true));
+            //My home-grown classifier
+            Classifier classifier = new ID3();
+
+            //Weka's classifier that doesn't take nominal data
+            //Id3 classifier = new Id3();
+            Evaluation eval = new Evaluation(dataSet);
+            final int folds = 10;
+            for (int n = 0; n < folds; n++) {
+                Instances train = dataSet.trainCV(folds, n);
+                Instances test = dataSet.testCV(folds, n);
+
+                Classifier clsCopy = Classifier.makeCopy(classifier);
+                clsCopy.buildClassifier(train);
+                eval.evaluateModel(clsCopy, test);
+            }
+
+            System.out.println(eval.toSummaryString("\n" + folds + " Fold Cross Validation\n==============================================="
+                    + "===================", false));
 
 
-    }
+        }
 }
